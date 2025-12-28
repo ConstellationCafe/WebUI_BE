34d4e69f1881350e2f9b171853b5a0a4d414ed95
BlackChihiro <blackayano00@gmail.com>
2025-12-29 08:04:25 +0900

    chore: 인증 필터 분리 등록

--- AuthServerPlatform/src/main/java/com/help/global/config/security/SecurityConfig.java
+++ AuthServerPlatform/src/main/java/com/help/global/config/security/SecurityConfig.java
@@ -1,9 +1,12 @@
-package com.help.authserver.global.config.security;
+package com.help.global.config.security;
 
-import java.util.List;
-
+import com.help.global.jwt.AuthServerJwtAuthFilter;
+import com.help.global.jwt.BackEndJwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,47 +16,65 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.help.authserver.global.jwt.JwtAuthenticationFilter;
-
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final AuthServerJwtAuthFilter authServerJwtAuthFilter;
+	private final BackEndJwtAuthFilter backEndJwtAuthFilter;
 	private final EmailVerificationFilter emailVerificationFilter;
 
-	// TODO: CSRF 토큰 사용 시 주석 해제
-	// @Bean
-	// public CsrfTokenRepository csrfTokenRepository() {
-	// 	HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-	// 	repository.setHeaderName("X-XSRF-TOKEN");
-	// 	return repository;
-	// }
+	@Value("${front.redirect-uri}")
+	private String redirectUri;
+
+	 @Bean
+	 public CsrfTokenRepository csrfTokenRepository() {
+	 	HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+	 	repository.setHeaderName("X-XSRF-TOKEN");
+	 	return repository;
+	 }
 
 	@Bean
+	@Order(1)
 	public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-		// http.csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository())
-		// 	.ignoringRequestMatchers("/login")
-		// )
-		http.csrf(AbstractHttpConfigurer::disable);
+		http.securityMatcher("/auth/**");
+
+		http.csrf(AbstractHttpConfigurer::disable);  // CSRF 비활성화 (JWT 기반)
 		http.sessionManagement(session -> session
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		);  // 세션 생성 x, JWT로만 인증
+		http.cors(corsCustomizer -> corsCustomizer.configurationSource(configurationSource()));  // 커스텀 CORS
+		http.formLogin(AbstractHttpConfigurer::disable);  // login 폼 기반 인증 x
+
+		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());  // 인가 설정
+		http.addFilterBefore(authServerJwtAuthFilter, ExceptionTranslationFilter.class)
+			.addFilterBefore(emailVerificationFilter, AuthServerJwtAuthFilter.class);  // 인증 필터 구성
+		return http.build();
+	}
+
+	@Bean
+	@Order(2)
+	public SecurityFilterChain backendChain(HttpSecurity http) throws Exception {
+		http.securityMatcher("/api/**");
+
+		http.csrf(AbstractHttpConfigurer::disable);
+		http.sessionManagement(session ->
+				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		);
-		http.cors(corsCustomizer -> corsCustomizer.configurationSource(configurationSource()));
-		http.formLogin(AbstractHttpConfigurer::disable);
-		http.authorizeHttpRequests((authorize) ->
-			authorize.requestMatchers("/**").permitAll()
-				.anyRequest().authenticated()
-		);
-		http.addFilterBefore(jwtAuthenticationFilter, ExceptionTranslationFilter.class)
-			.addFilterBefore(emailVerificationFilter, JwtAuthenticationFilter.class);
+
+		http.addFilterBefore(backEndJwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
 		return http.build();
 	}
 
@@ -67,10 +88,7 @@ public class SecurityConfig {
 		final CorsConfiguration configuration = new CorsConfiguration();
 
 		// 프론트엔드 서버 주소
-		// configuration.setAllowedOrigins(List.of(
-		// 	"http://192.168.25.93:5173"
-		// ));
-		configuration.setAllowedOriginPatterns(List.of("*"));
+		configuration.setAllowedOriginPatterns(List.of(redirectUri));
 		
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
