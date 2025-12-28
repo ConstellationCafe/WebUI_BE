46109138090a88c050a5c2979476cb7b04da04ad
BlackChihiro <blackayano00@gmail.com>
2025-12-29 08:12:39 +0900

    chore: Learning 테이블 데이터 조회 테스트

---
+++ AuthServerPlatform/src/test/java/com/help/backend/domain/repository/repository/LearningRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.help.backend.domain.repository.repository;
+
+import com.help.backend.domain.repository.entity.LearningEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class LearningRepositoryTest {
+    @Autowired
+    LearningRepository learningRepository;
+
+    @Test
+    void findByTeacherTest() {
+        // given
+        LearningEntity e1 = LearningEntity.of("수학", "math", "1234");
+        LearningEntity e2 = LearningEntity.of("영어", "english", "1234");
+        LearningEntity e3 = LearningEntity.of("과학", "lee", "4567");
+
+        learningRepository.save(e1);
+        learningRepository.save(e2);
+        learningRepository.save(e3);
+
+        // when
+        List<LearningEntity> result = learningRepository.findByTeacher("1234");
+
+        // then
+        assertThat(result).hasSize(2);
+        assertThat(result)
+                .extracting(LearningEntity::getTeacher)
+                .containsOnly("1234");
+    }
+}
