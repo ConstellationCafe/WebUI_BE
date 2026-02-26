ab9b3f9b7d48cdd2d8e1f7b5ebb2cdb1aa3019ca
BlackChihiro <blackayano00@gmail.com>
2026-02-26 16:46:55 +0900

    feat: 음악추천 기능 추가

---
+++ AuthServerPlatform/src/main/java/com/help/backend/domain/music/repository/MusicRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.help.backend.domain.music.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.ParameterMode;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.StoredProcedureQuery;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MusicRepositoryImpl implements MusicRepositoryCustom {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public String callMusicProcedure(String cardType,
+                                    String membershipID,
+                                    String videoId) {
+
+        StoredProcedureQuery sp = em.createStoredProcedureQuery("ChatBot.recommend_music");
+
+        sp.registerStoredProcedureParameter("card_type", String.class, ParameterMode.IN);
+        sp.registerStoredProcedureParameter("membershipID", String.class, ParameterMode.IN);
+        sp.registerStoredProcedureParameter("videoId", String.class, ParameterMode.IN);
+        sp.registerStoredProcedureParameter("recommend_result", String.class, ParameterMode.OUT);
+
+        sp.setParameter("card_type", cardType);
+        sp.setParameter("membershipID", membershipID);
+        sp.setParameter("video_id", videoId);
+
+        sp.execute();
+
+        return (String) sp.getOutputParameterValue("recommend_result");
+    }
+}
