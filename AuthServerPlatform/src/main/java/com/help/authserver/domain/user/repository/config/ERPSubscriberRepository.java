d3aa07286bb326b8c800f0b931dbc1d697a1bed3
BlackChihiro <blackayano00@gmail.com>
2026-08-16 12:23:33 +0900

    chore: guildId로 ErpSubscriber 조회하는 기능 추가

--- AuthServerPlatform/src/main/java/com/help/authserver/domain/user/repository/config/ERPSubscriberRepository.java
+++ AuthServerPlatform/src/main/java/com/help/authserver/domain/user/repository/config/ERPSubscriberRepository.java
@@ -4,8 +4,10 @@ import com.help.authserver.domain.user.entity.config.ErpSubscriber;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ERPSubscriberRepository extends JpaRepository<ErpSubscriber, String> {
     List<ErpSubscriber> findByGuildIdIn(List<String> guildIds);
-    List<ErpSubscriber> findByDiscordId(String discordId);
+    List<ErpSubscriber> findByDiscordId(String discordId);  // 임시로 만든 기능
+    Optional<ErpSubscriber> findByGuildId(Long guildId);
 }
