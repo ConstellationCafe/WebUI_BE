60e98e0da0cc797945e9402ba3672d4aef09a1dc
BlackChihiro <blackayano00@gmail.com>
2026-08-17 21:05:57 +0900

    chore: 학생 명단 조회 기능 추가

--- AuthServerPlatform/src/main/java/com/help/erpweb/domain/academy/repository/StudentRepository.java
+++ AuthServerPlatform/src/main/java/com/help/erpweb/domain/academy/repository/StudentRepository.java
@@ -2,12 +2,26 @@ package com.help.erpweb.domain.academy.repository;
 
 import com.help.erpweb.domain.academy.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository
         extends JpaRepository<Student, Integer> {
+    @Query("""
+        SELECT s
+        FROM Student s
+        JOIN FETCH s.academyClass c
+        JOIN FETCH c.academy a
+        WHERE a.id = :academyId
+          AND c.id = :classId
+    """)
+    List<Student> findByAcademyIdAndClassId(
+            @Param("academyId") Integer academyId,
+            @Param("classId") Integer classId
+    );
 
     List<Student> findByAcademyClass_Academy_Id(
             Integer academyId
